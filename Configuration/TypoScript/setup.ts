7b3fb3a2c02edbce25028c55e946fc67de0a6d96
Tim Büschken <bueschken@team-digital.de>
2020-01-09 12:02:39 +0100

    [UPDATE] Add Favicon to custom template

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -15,6 +15,95 @@ page {
 
 	// HeaderData
 	headerData {
+	2 = TEXT
+		2.value = <meta name="format-detection" content="telephone=no">
+		5 = COA
+		5 {
+			// Apple touch icons
+			5 = IMG_RESOURCE
+			5 {
+				file = {$custom.images}favicon_256.png
+				file {
+					width = 57c
+					height = 57c
+				}
+
+				stdWrap.dataWrap = <link rel="apple-touch-icon" sizes="{TSFE:lastImgResourceInfo|0}x{TSFE:lastImgResourceInfo|1}" href="|">
+			}
+
+			10 < .5
+			10.file {
+				width = 76c
+				height = 76c
+			}
+
+			15 < .5
+			15.file {
+				width = 114c
+				height = 114c
+			}
+
+			20 < .5
+			20.file {
+				width = 152c
+				height = 152c
+			}
+
+			25 < .5
+			25.file {
+				width = 180c
+				height = 180c
+			}
+
+			// ...
+
+			// Android icons
+			30 < .5
+			30 {
+				file {
+					width = 16c
+					height = 16c
+				}
+
+				stdWrap.dataWrap = <link rel="icon" type="image/png" sizes="{TSFE:lastImgResourceInfo|0}x{TSFE:lastImgResourceInfo|1}" href="|">
+			}
+
+			35 < .5
+			35.file {
+				width = 32c
+				height = 32c
+			}
+
+			40 < .5
+			40.file {
+				width = 96c
+				height = 96c
+			}
+
+			45 < .5
+			45.file {
+				width = 160c
+				height = 160c
+			}
+
+			50 < .5
+			50.file {
+				width = 196c
+				height = 196c
+			}
+
+			// Microsoft Application icons
+			55 < .5
+			55 {
+				file {
+					width = 70c
+					height = 70c
+				}
+
+				stdWrap.dataWrap = <meta name="msapplication-square{TSFE:lastImgResourceInfo|0}x{TSFE:lastImgResourceInfo|1}logo" content="|"/>
+			}
+
+		}
 		// Werte ab 10.000
 	}
 
