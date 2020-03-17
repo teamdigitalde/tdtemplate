3bf1a17fe05cf319625e2368e68ea6c43bd70a38
tb96 <bueschken@team-digital.de>
2020-03-17 11:11:26 +0100

    [ADD] Lade Default.yaml von Tdtemplate und nicht mehr vom Sitepackage

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ defined('TYPO3_MODE') || die();
 /***************
  * Add default RTE configuration
  */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sitepackage'] = 'EXT:sitepackage/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sitepackage'] = 'EXT:tdtemplate/Configuration/RTE/Default.yaml';
 
 // Import Tasks
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'TeamDigital\\Tdtemplate\\Command\\UpdateCommandController';
