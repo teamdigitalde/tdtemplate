e17e41985e3f77fa318d2723f63bc01e65f4821d
TBueschken <bueschken@team-digital.de>
2020-04-15 15:58:33 +0200

    [UPDATE] Change $_EXTKEY against the Extensionkey

--- ext_tables.php
+++ ext_tables.php
@@ -2,18 +2,18 @@
 defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/page.ts">'
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tdtemplate/Configuration/PageTS/page.ts">'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sitepackage_domain_model_anchor');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'TeamDigital.tdtemplate',
     'Configuration/TypoScript',
     'tdtemplate'
 );
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
+    'TeamDigital.tdtemplate',
     'Configuration/TypoScript',
     'tdtemplate'
 );
